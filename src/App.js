21aaf5e9cf91aeade7e17ef8e341223ddf4eb592
RebecaConte <becaconte@gmail.com>
2021-05-13 15:25:48 +0100

    sign up page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Switch, Route } from "react-router-dom";
-import AboutUs from "./components/AboutUs"
+import AboutUs from "./components/AboutUs";
+import SignIn from "./components/users/SignIn";
+import SignUp from "./components/users/SignUp";
 import NotFound from "./components/404Page/NotFound";
 
 
@@ -9,7 +11,17 @@ class App extends Component {
     return (
       <div>
         <Switch>
-         
+
+          <Route path="/signup" render={() => {
+            return <SignUp />
+          }} />
+
+          <Route path="/signin" render={() => {
+            return <SignIn />
+          }} />
+
+
+
 
           <Route path="/aboutus" render={() => {
             return <AboutUs />
---
+++ src/components/users/SignUp.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class SignUp extends Component {
+    render() {
+        return (
+            <form >
+                <div>
+                    <label>Username</label>
+                    <input type="text" name="username" />
+                </div>
+                <div >
+                    <label >Email address</label>
+                    <input type="email" name="email" />
+                    <small className="form-text text-muted">Don't worry, we'll never share your email with anyone else.</small>
+                </div>
+                <div>
+                    <label >Password</label>
+                    <input name="password" type="password" />
+                </div>
+                <button type="submit" >Submit</button>
+            </form>
+        )
+    }
+}
+
+
+export default SignUp
